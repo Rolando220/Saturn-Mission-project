755d9c80e3f8b8b0831091bdab54680de9a44950
Ettore-Bioli01 <ettorebioli@gmail.com>
2025-12-19 22:31:28 +0100

    Aggiunta viaggio verso marte e modifica debug per visualizzarlo

--- MatLab/Debug/DEBUG_E_M.m
+++ MatLab/Debug/DEBUG_E_M.m
@@ -18,9 +18,9 @@ try
     
     
     % Punti chiave
-    plot(r_sat_earth_sp(1), r_sat_earth_sp(2), 'ro', 'MarkerFaceColor', 'r', 'DisplayName', 'Uscita SOI Terra');
+    plot(r_sat_earthfb_sp(1), r_sat_earthfb_sp(2), 'ro', 'MarkerFaceColor', 'r', 'DisplayName', 'Uscita SOI Terra');
     plot(r_sat_interplanetary_earth_mars(1), r_sat_interplanetary_earth_mars(2), 'kx', 'MarkerSize', 8, 'LineWidth', 2, 'DisplayName', 'Sat end');
-    plot(r_earth_sp(1), r_earth_sp(2), 'bo', 'MarkerFaceColor', 'b', 'DisplayName', 'Terra (alla partenza)');
+    plot(r_earthfb_sp(1), r_earthfb_sp(2), 'bo', 'MarkerFaceColor', 'b', 'DisplayName', 'Terra (alla partenza)');
     %plot(r_mars_traj(1, 1), r_mars_traj(2, 1), 'mo', 'MarkerFaceColor', 'w', 'DisplayName', 'Marte (Start)');
     plot(r_mars_arr(1), r_mars_arr(2), 'mo', 'MarkerFaceColor', 'm', 'DisplayName', 'Marte (arrivo)');
     
@@ -28,18 +28,18 @@ try
     theta = linspace(0,2*pi,240)';
     earth_soi_au = soi_earth / au;
     mars_soi_au = soi_mars / au;
-    plot(r_earth_sp(1) + earth_soi_au*cos(theta), r_earth_sp(2) + earth_soi_au*sin(theta), 'b--', 'DisplayName', 'SOI Terra (proj)');
+    plot(r_earthfb_sp(1) + earth_soi_au*cos(theta), r_earthfb_sp(2) + earth_soi_au*sin(theta), 'b--', 'DisplayName', 'SOI Terra (proj)');
     plot(r_mars_arr(1) + mars_soi_au*cos(theta), r_mars_arr(2) + mars_soi_au*sin(theta), 'm--', 'DisplayName', 'SOI Marte (proj)');
 
     % Draw velocity direction arrows (scaled for visibility)
     try
         arrow_days = 5; % represent ~5 days displacement for visibility
         scale_seconds = 3600 * 24 * arrow_days;
-        v_earth_vec = v_earth_sp * scale_seconds; % AU
+        v_earth_vec = v_earthfb_sp * scale_seconds; % AU
         v_mars_vec = v_mars_arr * scale_seconds;  % AU
-        quiver(r_earth_sp(1), r_earth_sp(2), v_earth_vec(1), v_earth_vec(2), 0, 'b', 'LineWidth', 1.4, 'MaxHeadSize', 0.5);
+        quiver(r_earthfb_sp(1), r_earthfb_sp(2), v_earth_vec(1), v_earth_vec(2), 0, 'b', 'LineWidth', 1.4, 'MaxHeadSize', 0.5);
         quiver(r_mars_arr(1), r_mars_arr(2), v_mars_vec(1), v_mars_vec(2), 0, 'm', 'LineWidth', 1.4, 'MaxHeadSize', 0.5);
-        text(r_earth_sp(1) + v_earth_vec(1)*0.06, r_earth_sp(2) + v_earth_vec(2)*0.06, 'v_{Earth} (scaled)', 'Color', 'b');
+        text(r_earthfb_sp(1) + v_earth_vec(1)*0.06, r_earthfb_sp(2) + v_earth_vec(2)*0.06, 'v_{Earth} (scaled)', 'Color', 'b');
         text(r_mars_arr(1) + v_mars_vec(1)*0.06, r_mars_arr(2) + v_mars_vec(2)*0.06, 'v_{Mars} (scaled)', 'Color', 'm');
     catch
         % non-critico se quiver fallisce
